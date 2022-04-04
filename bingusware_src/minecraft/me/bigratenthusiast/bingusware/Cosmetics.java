d8cfc711e06f795057e7fd60f5bab4fc9ca98c94
Bigrat Enthusiast <77901819+bigratenthusiast@users.noreply.github.com>
2022-04-03 20:54:23 -0400

    fix

--- bingusware_src/minecraft/me/bigratenthusiast/bingusware/Cosmetics.java
+++ bingusware_src/minecraft/me/bigratenthusiast/bingusware/Cosmetics.java
@@ -1,14 +1,30 @@
 package me.bigratenthusiast.bingusware;
 
-public class Cosmetics {
-    public static String protocol = "https";
-    public static String mojangSkinApi = "://mc-heads.net/skin/", mojangCloakApi = "://mc-heads.net/cape/";
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
+public class Cosmetics {
     public static String getSkin(String username) {
-        return protocol + mojangSkinApi + username + ".png";
+        return "https://crafatar.com/skins/" + getUUID(username);
     }
 
     public static String getCape(String username) {
-        return protocol + mojangCloakApi + username;
+        return "https://crafatar.com/capes/" + getUUID(username);
+    }
+
+    public static String getUUID(String username) {
+        String UUID = "037a1727-407c-45fd-ab29-19382764e896"; // error uuid
+        try  {
+            BufferedReader raw = new BufferedReader(new InputStreamReader(new URL("https://api.mojang.com/users/profiles/minecraft/" + username).openStream()));
+            String input = "";
+            for (String i;(i=raw.readLine())!=null;input+=i);
+            UUID = input.split("id\":\"")[1].split("\"")[0];
+        } catch (Exception e) {
+            System.out.println("Unable to get UUID of player " + username);
+        }
+        System.out.println(UUID);
+        return UUID;
     }
 }
