3b6e3323dd0434053a1dcfbee6df55b68b36f128
Bigrat Enthusiast <77901819+bigratenthusiast@users.noreply.github.com>
2022-07-03 20:17:13 -0400

    Delete bingusware_src/minecraft directory

--- bingusware_src/minecraft/me/bigratenthusiast/bingusware/module/modules/DummyC.java
+++
@@ -1,8 +0,0 @@
-package me.bigratenthusiast.bingusware.module.modules;
-
-import me.bigratenthusiast.bingusware.module.Module;
-import org.lwjgl.input.Keyboard;
-
-public class DummyC extends Module{
-    public DummyC() { super("DummyC", Keyboard.KEY_L, Type.MISC); }
-}
